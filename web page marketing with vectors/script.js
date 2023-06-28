726c912c77daed25f1c84eddadb688619912334e
Mobina <126059447+mobina-aty@users.noreply.github.com>
2023-06-28 14:14:00 +0330

    web on page

---
+++ web page marketing with vectors/script.js
@@ -0,0 +1,11 @@
+﻿// BOM, DOM
+
+let h1Elem = document.getElementById('title')
+
+h1Elem.setAttribute('class', 'js-text')
+
+let liElems = document.getElementsByClassName('list-item')
+
+console.log(liElems[0].setAttribute('id', 'about'))
+
+// console.log(h1Elem.getAttribute('class'))
